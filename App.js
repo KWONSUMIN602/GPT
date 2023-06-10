f6b0b93bc9d2b596018264f5351171c63d7a1654
kwonsumin602 <ksm8849@gmail.com>
2023-06-10 19:15:21 +0900

    Update expo speech and reset input value

--- App.js
+++ App.js
@@ -1,19 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 import { GiftedChat } from 'react-native-gifted-chat';
+import * as Speech from 'expo-speech';
 
 export default function App() {
 	const [messages, setMessages] = useState([]);
 	const [inputMessage, setInputMessage] = useState('');
-	const [outputMessage, setoOutputMessage] = useState('Result to be shown here!');
+	const [outputMessage, setOutputMessage] = useState('Result to be shown here!');
 	const [outputImage, setOutputImage] = useState('Result to be shown here!');
 
-	/**
-	 * open ai api 의 chat 함수
-	 */
 	const handleButtonClick = () => {
+		console.log(inputMessage);
+		if (inputMessage.toLocaleLowerCase().startsWith('generate image')) {
+			generateImages();
+			return;
+		} else {
+			generateText();
+			return;
+		}
+	};
+
+	/**
+	 * open ai api 의 textchat 함수
+	 */
+	const generateText = () => {
 		console.log('SEND : ', inputMessage);
 
 		const message = {
@@ -39,7 +51,7 @@ export default function App() {
 			.then((res) => res.json())
 			.then((data) => {
 				console.log(data.choices[0].messafe.content.trim());
-				setoOutputMessage(data.choices[0].messafe.content.trim());
+				setOutputMessage(data.choices[0].messafe.content.trim());
 
 				const message = {
 					_id: Math.random().toString(36).substring(7),
@@ -49,6 +61,9 @@ export default function App() {
 				};
 
 				setMessages((previousMessages) => GiftedChat.append(previousMessages, [message]));
+
+				options = {};
+				Speech.speak(data.choices[0].messafe.content, options);
 			})
 			.catch((error) => {
 				console.log('에러 :', error);
@@ -61,6 +76,8 @@ export default function App() {
 
 				setMessages((previousMessages) => GiftedChat.append(previousMessages, [message]));
 			});
+
+		setInputMessage('');
 	};
 
 	/**
@@ -121,6 +138,8 @@ export default function App() {
 
 				setMessages((previousMessages) => GiftedChat.append(previousMessages, [message]));
 			});
+
+		setInputMessage('');
 	};
 
 	const handleTextInput = (text) => {
@@ -129,59 +148,70 @@ export default function App() {
 	};
 
 	return (
-		<View style={{ flex: 1 }}>
-			<View style={{ flex: 1, justifyContent: 'center', padding: 10 }}>
-				{/* <Text>{outputImage}</Text> */}
-				{/* <Text>{outputMessage}</Text> */}
-				<GiftedChat
-					messages={messages}
-					renderInputToolbar={() => {}}
-					user={{ _id: 1 }}
-					minInputToolbarHeight={0}
-				/>
-			</View>
-
-			<View style={{ flexDirection: 'row', marginHorizontal: 10, marginBottom: 20 }}>
-				<View
-					style={{
-						flex: 1,
-						backgroundColor: 'white',
-						borderRadius: 10,
-						borderColor: 'grey',
-						borderWidth: 1,
-						height: 60,
-						marginHorizontal: 10,
-						justifyContent: 'center',
-						paddingHorizontal: 10,
-					}}
-				>
-					<TextInput placeholder="Enter your question" onChangeText={handleTextInput} />
+		<ImageBackground
+			source={require('./assets/bg.webp')}
+			resizeMode="cover"
+			style={{ flex: 1, width: '100%', height: '100%' }}
+		>
+			<View style={{ flex: 1 }}>
+				<View style={{ flex: 1, justifyContent: 'center', padding: 10 }}>
+					{/* <Text>{outputImage}</Text> */}
+					{/* <Text>{outputMessage}</Text> */}
+					<GiftedChat
+						messages={messages}
+						renderInputToolbar={() => {}}
+						user={{ _id: 1 }}
+						minInputToolbarHeight={0}
+					/>
 				</View>
 
-				<TouchableOpacity onPress={generateImages}>
+				<View style={{ flexDirection: 'row', marginHorizontal: 10, marginBottom: 20 }}>
 					<View
 						style={{
-							backgroundColor: 'green',
-							padding: 5,
-							marginRight: 10,
-							marginBottom: 10,
-							borderRadius: '100%',
-							width: 60,
+							flex: 1,
+							backgroundColor: 'white',
+							borderRadius: 10,
+							borderColor: 'grey',
+							borderWidth: 1,
 							height: 60,
+							marginHorizontal: 10,
 							justifyContent: 'center',
+							paddingHorizontal: 10,
 						}}
 					>
-						<MaterialIcons
-							name="send"
-							size={30}
-							color={'white'}
-							style={{ marginHorizontal: 'auto' }}
+						<TextInput
+							placeholder="Enter your question"
+							onChangeText={handleTextInput}
+							style={{ border: 'none' }}
+							value={inputMessage}
 						/>
 					</View>
-				</TouchableOpacity>
-			</View>
 
-			<StatusBar style="auto" />
-		</View>
+					<TouchableOpacity onPress={handleButtonClick}>
+						<View
+							style={{
+								backgroundColor: '#2C528C',
+								padding: 5,
+								marginRight: 10,
+								marginBottom: 10,
+								borderRadius: '100%',
+								width: 60,
+								height: 60,
+								justifyContent: 'center',
+							}}
+						>
+							<MaterialIcons
+								name="send"
+								size={30}
+								color={'white'}
+								style={{ marginHorizontal: 'auto' }}
+							/>
+						</View>
+					</TouchableOpacity>
+				</View>
+
+				<StatusBar style="auto" />
+			</View>
+		</ImageBackground>
 	);
 }
